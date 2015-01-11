0b4f282831fabca08b8b72f3c375a036a7f1ad8f
MySmartFridge <mysmartfridge@gmail.com>
2015-01-11 18:47:58 -0500

    edit
    edit

---
+++ MSFParse.js
@@ -0,0 +1,46 @@
+function signUpUser() {
+
+      Parse.initialize("kYnpHfKQqce0BYrCOJj3jB0Jz6HxXDjf5f8ggJLL", "RCytSIBImKnGWbgn0KZdGwbdm9LiOmioo87xi1Wv");
+          var user = new Parse.User();
+
+	        var username = $("#signup-username").val();
+	        var email = $("#signup-email").val();
+	        var password = $("#signup-password").val();
+
+	        user.set("username", username);
+			user.set("password", password);
+			user.set("email", email);
+
+	        user.signUp(null, {
+		        success: function(user) {
+			        alert("You successfully signed up!");
+			        var currentUser = Parse.User.current();  
+			        	if(currentUser) {
+	  					// TODO: Bring them to the hidden "logged in only" page
+  					}  
+			     },
+			    error: function(user, error) {
+				    alert("Error: " + error.code + " " + error.message);
+				}
+		});
+}
+
+function signInUser() {
+
+			Parse.initialize("kYnpHfKQqce0BYrCOJj3jB0Jz6HxXDjf5f8ggJLL", "RCytSIBImKnGWbgn0KZdGwbdm9LiOmioo87xi1Wv");
+			var username = $("#login-username").val();
+			var password = $("#login-password").val();
+
+			Parse.User.logIn(username, password, {
+  				success: function(user) {
+  					alert("You successfully logged in!");
+  					var currentUser = Parse.User.current();
+  					if(currentUser) {
+	  					// TODO: Bring them to the hidden "logged in only" page
+  					}  
+  				},
+ 				 error: function(user, error) {
+ 				 	alert("Error: " + error.code + " " + error.message);
+			 	}
+		});
+}
